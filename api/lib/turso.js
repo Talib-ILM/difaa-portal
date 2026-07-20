07af8cbde329f5cfb227460b2ef30f741b46345f
Talib-ILM <Talib-ILM@users.noreply.github.com>
2026-07-20 20:09:52 +0000

    Simplify to single HTML file using Turso HTTP API directly

--- api/lib/turso.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "@tursodatabase/serverless";
-
-export function getTurso() {
-  return connect({
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  });
-}
